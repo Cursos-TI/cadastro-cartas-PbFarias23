0d7107cd731356d09704fd63db86cff969958d21
PbFarias23 <pbfc1906@gmail.com>
2025-10-22 13:52:27 +0000

    menu01

---
+++ super trunfo - aventureiro - menu inteirativo.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main (){
+
+    char nome1[50] = "Rio de Janeiro";
+    char nome2[50] = "São Paulo";
+    int populacao1 = 161000000;
+    int populacao2 = 215000000;
+    float area1 = 1200;
+    float area2 = 248219;
+    float pib1 = 10150000;
+    float pib2 = 30130000;
+    int pontos1 = 120;
+    int pontos2 = 37;
+    float densidade1 = populacao1 / area1;
+    float densidade2 = populacao2 / area2;
+    int opcao;
+
+    //menu
+    printf("$$$ --- Batalha dos Estados --- $$$\n\n");
+
+}
